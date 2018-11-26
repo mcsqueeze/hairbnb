8bc33de5f1ff6b144fb52157cfa608596afc04d8
Anna Fuchtman <fuchtman1@gmail.com>
2018-11-26 17:36:49 +0100

    wigs-controller edit

--- app/controllers/wigs_controller.rb
+++ app/controllers/wigs_controller.rb
@@ -1,22 +1,45 @@
 class WigsController < ApplicationController
   def index
-  end
-
-  def show
+    @wigs = Wig.all
   end
 
   def new
+    @wig = Wig.new
   end
 
   def create
+    @wig = Wig.new(wig_params)
+    if @wig.save
+      redirect_to wigs_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @wig = Wig.find(params[:id])
   end
 
   def edit
+    @wig = Wig.find(params[:id])
   end
 
   def update
+    if @wig.update(wig_params)
+      redirect_to wigs_path
+    else
+      render :edit
   end
 
   def destroy
+    Wig.find(params[:id]).destroy
+    flash[:success] = "Wig Deleted"
+    redirect_to wigs_path
   end
 end
+
+  private
+
+  def wig_params
+    params.require(:wig).permit(:title, :photo, :description, :location, :policies, :price)
+  end
