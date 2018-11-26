e1ecbfa83df3e00ea7d21efdf18d2df1144a95d4
Margaux Delayre <mcsqueeze@gmail.com>
2018-11-26 17:42:38 +0100

    bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,20 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = Booking.all
   end
 
   def show
-  end
-
-  def new
+    @booking = Booking.find(params[:id])
+    @new_booking = Booking.new
   end
 
   def create
+    @new_booking = Booking.new(booking_params)
+    if @new_booking.save
+      redirect_to flats_path
+    else
+      render :show
+    end
   end
 
   def edit
