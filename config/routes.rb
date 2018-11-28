a55438a066592b81d74d8868028198f6736d5c5d
Margaux Delayre <mcsqueeze@gmail.com>
2018-11-28 16:41:50 +0100

    last changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'wigs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- resources :wigs do
+ resources :wigs  do
   resources :bookings, only: [ :new, :create ]
  end
  resources :bookings, only:  [ :show, :edit, :update, :destroy, :index ]
