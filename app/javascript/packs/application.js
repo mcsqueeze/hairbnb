e3f345d422aa2d34f62c78ba09b1145c1710f83c
Margaux Delayre <mcsqueeze@gmail.com>
2018-11-28 16:47:09 +0100

    last last changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,27 +1,3 @@
 import "bootstrap";
 
 
-
-    const calendar = document.getElementById('calendar');
-    flatpickr(calendar, {
-    enableTime:      true,
-    altInput:        true,
-    minDate:         "today",
-    altFormat:       "F j, Y at h:i K",
-    dateFormat:      "Y-m-d H:i",
-    defaultHour:     23,
-    defaultMinute:   59,
-    minuteIncrement: 1
-    });
-
-    const calendar1 = document.getElementById('calendar1');
-    flatpickr(calendar1, {
-    enableTime:      true,
-    altInput:        true,
-    minDate:         "today",
-    altFormat:       "F j, Y at h:i K",
-    dateFormat:      "Y-m-d H:i",
-    defaultHour:     23,
-    defaultMinute:   59,
-    minuteIncrement: 1
-    });
--- app/javascript/packs/calendar.js
+++
@@ -1,6 +0,0 @@
-  function calendar() {
-
-}
-export  calendar;
-
-
