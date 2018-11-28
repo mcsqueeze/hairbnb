1aa557820f7fa03727003fe3d1b4ad4c296e1f4b
Anna Fuchtman <fuchtman1@gmail.com>
2018-11-28 11:05:16 +0100

    added cards to indexpage

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 require 'date'
 
 u1 = User.create(email: "margaux@gmail.com", password: "margaux", usertype: "lender")
-w1 = Wig.create(title: "Red", user: User.first, description: "red hair", price: "15", photo: "http://res.cloudinary.com/dtqkxojhd/image/upload/v1543312837/20170927163904_28607.jpg")
+w1 = Wig.create(title: "Ruby Red", user: User.first, description: "long, gorgeous, way, red hair", price: "15.00", photo: "http://res.cloudinary.com/dtqkxojhd/image/upload/v1543393648/20170927163904_28607.jpg")
 d1 = Date.new(2018,11,3)
 d2 = Date.new(2018,11,4)
 Booking.create(start_date: d1, end_date: d2, user: User.first, wig: w1)
